3bda7d5799c09d4be7d3c1e9afd8410c0493b6ff
Fabien Roy <exilednarwal28@gmail.com>
2016-12-08 20:47:13 -0500

    Formatting

---
+++ TP1-VB/FormManipulerUtilisateur.vb
@@ -0,0 +1,273 @@
+﻿Option Strict On
+Option Explicit On
+Public Class FormManipulerItem
+  Private DataTableTrav As DataTableTravailleur
+
+  Private FormulaireMode As String ' Ajout ou modifications
+  Private IDModif As Int32
+
+  Public Sub New(ByRef DataTableTrav As DataTableTravailleur)
+    InitializeComponent()
+    Me.DataTableTrav = DataTableTrav
+    Me.FormulaireMode = ""
+    Me.IDModif = -1
+  End Sub
+
+  Public Sub SetFormulaireMode(ByVal Mode As String)
+    ' Méthode pour appliquer un mode du formulaire
+
+    Me.FormulaireMode = Mode
+
+    ' On change l'apparence du formulaire en fonction du mode du formulaire
+    Select Case Me.FormulaireMode
+      Case "Ajout"
+        Me.Text = "Ajout d'un item"
+        Me.ButtonItemsAjoutAction.Text = "Ajouter"
+        Me.ButtonItemsAjoutAction.BackColor = Color.SeaGreen
+      Case "Modification"
+        Me.Text = "Modification d'un item"
+        Me.ButtonItemsAjoutAction.Text = "Modifier"
+        Me.ButtonItemsAjoutAction.BackColor = Color.DarkOrange
+        If Not Me.IDModif = -1 Then
+          Me.RemplirChampsRangee()
+        End If
+    End Select
+  End Sub
+
+  Public Sub SetIDModif(ByVal IDModif As Int32)
+    Me.IDModif = IDModif
+  End Sub
+
+  Private Sub ButtonItemsAjoutAction_Click(sender As Object, e As EventArgs) Handles ButtonItemsAjoutAction.Click
+    ' Lorsque l'utilisateur clique sur ce bouton, on vérifie le contenu des champs et, si tout est ok, on construit un nouveau DataRow avec
+
+    If VerifierChamp() Then
+      ' On vérifie quel est le mode du formulaire
+      Select Case Me.FormulaireMode
+        Case "Ajout"
+          Me.InsertInventaireComplet()
+          Me.Hide()
+          Me.ViderFormulaire()
+        Case "Modification"
+          If Me.ModifierRangee() Then
+            Me.Hide()
+            Me.ViderFormulaire()
+          End If
+      End Select
+    End If
+  End Sub
+
+  Private Sub ButtonItemsAjoutAnnuler_Click(sender As Object, e As EventArgs) Handles ButtonItemsAjoutAnnuler.Click
+    Me.ViderFormulaire()
+    Me.Hide()
+  End Sub
+
+  ' Méthode pour insérer à l'inventaire complet
+  Private Sub InsertInventaireComplet()
+    DataTableTrav.InsertInventaireComplet(
+      TextBoxItemAjoutCodeProduit.Text,
+      TextBoxItemAjoutDesc.Text,
+      TextBoxItemAjoutEmp.Text,
+      TextBoxItemAjoutCat.Text,
+      TextBoxItemAjoutDep.Text,
+      TextBoxItemAjoutFournCode.Text,
+      TextBoxItemAjoutFournNom.Text,
+      CDbl(TextBoxItemAjoutPrixV.Text),
+      CDbl(TextBoxItemAjoutPrixA.Text),
+      CInt(TextBoxItemAjoutQt.Text))
+  End Sub
+
+  ' Méthode pour modifier dans l'inventaire complet
+  Private Sub UpdateInventaireComplet()
+    DataTableTrav.UpdateInventaireComplet(
+      Me.IDModif,
+      TextBoxItemAjoutCodeProduit.Text,
+      TextBoxItemAjoutDesc.Text,
+      TextBoxItemAjoutEmp.Text,
+      TextBoxItemAjoutCat.Text,
+      TextBoxItemAjoutDep.Text,
+      TextBoxItemAjoutFournCode.Text,
+      TextBoxItemAjoutFournNom.Text,
+      CDbl(TextBoxItemAjoutPrixV.Text),
+      CDbl(TextBoxItemAjoutPrixA.Text),
+      CInt(TextBoxItemAjoutQt.Text))
+  End Sub
+
+  ' Méthode pour ajouter un nouveau Datarow
+  Private Function ModifierRangee() As Boolean
+
+    ' On s'assure que les modifications sont correctes
+    If MsgBox(
+          "Êtes-vous certain de vos modifications?",
+          MsgBoxStyle.YesNo,
+          "Attention!") = DialogResult.Yes Then
+
+      Me.UpdateInventaireComplet()
+
+      ' On retourne vrai, pour cacher le formulaire
+      Return True
+    End If
+
+    ' Si l'utilisateur n'est pas content de ses modifications, on retourne faux pour rester le formulaire ouvert
+    Return False
+  End Function
+
+  Private Sub ViderFormulaire()
+    ' Méthode pour vider l'intégralité du formulaire
+
+    TextBoxItemAjoutCodeProduit.Text = ""
+    TextBoxItemAjoutDesc.Text = ""
+    TextBoxItemAjoutEmp.Text = ""
+    TextBoxItemAjoutCat.Text = ""
+    TextBoxItemAjoutFournCode.Text = ""
+    TextBoxItemAjoutDep.Text = ""
+    TextBoxItemAjoutFournNom.Text = ""
+    TextBoxItemAjoutPrixA.Text = ""
+    TextBoxItemAjoutPrixV.Text = ""
+    TextBoxItemAjoutQt.Text = ""
+  End Sub
+
+  ' Méthode pour remplir l'intégralité du formulaire avec une rangée
+  Private Sub RemplirChampsRangee()
+    Dim Donnees As TP1_VB.SelectInventaireCompletSingle_Result = Me.DataTableTrav.GetDataInventaireCompletSingle(IDModif)
+
+    MsgBox(Donnees.ToString())
+
+    With Donnees
+      TextBoxItemAjoutCodeProduit.Text = .Code_de_produit
+      TextBoxItemAjoutDesc.Text = .Description
+      TextBoxItemAjoutEmp.Text = .Emplacement
+      TextBoxItemAjoutFournNom.Text = .Nom_de_fournisseur
+      TextBoxItemAjoutFournCode.Text = .Code_de_fournisseur
+      TextBoxItemAjoutCat.Text = .Catégorie
+      TextBoxItemAjoutDep.Text = .Département
+      TextBoxItemAjoutPrixA.Text = CStr(.Prix_d_achat)
+      TextBoxItemAjoutPrixV.Text = CStr(.Prix_de_vente)
+      TextBoxItemAjoutQt.Text = CStr(.Quantité)
+    End With
+  End Sub
+
+  Private Function VerifierChamp() As Boolean
+    ' Fonction qui vérifie si tous les champs sont ok
+
+    Dim Resultat As Boolean = True
+
+    If TextBoxItemAjoutCodeProduit.Text = "" Then
+      LabelItemAjoutCodeProduit.ForeColor = Color.Red
+      Resultat = False
+    End If
+
+    If TextBoxItemAjoutDesc.Text = "" Then
+      LabelItemAjoutDesc.ForeColor = Color.Red
+      Resultat = False
+    End If
+
+    If TextBoxItemAjoutEmp.Text = "" Then
+      LabelItemAjoutEmp.ForeColor = Color.Red
+      Resultat = False
+    End If
+
+    If TextBoxItemAjoutFournNom.Text = "" Then
+      LabelItemAjoutFournNom.ForeColor = Color.Red
+      Resultat = False
+    End If
+
+    If TextBoxItemAjoutFournCode.Text = "" Then
+      LabelItemAjoutFournCode.ForeColor = Color.Red
+      Resultat = False
+    End If
+
+    If TextBoxItemAjoutDep.Text = "" Then
+      LabelItemAjoutDepNom.ForeColor = Color.Red
+      Resultat = False
+    End If
+
+    If TextBoxItemAjoutCat.Text = "" Then
+      LabelItemAjoutCatNom.ForeColor = Color.Red
+      Resultat = False
+    End If
+
+    If Not IsNumeric(TextBoxItemAjoutPrixA.Text) Then
+      LabelItemAjoutPrixA.ForeColor = Color.Red
+      Resultat = False
+    End If
+
+    If Not IsNumeric(TextBoxItemAjoutPrixV.Text) Then
+      LabelItemAjoutPrixV.ForeColor = Color.Red
+      Resultat = False
+    End If
+
+    ' La valeur suivante est inutile, mais il est nécéssaire d'envoyer une valeur integer ByRef à Int32.TryParse()
+    Dim ItemAjoutQt As Integer
+    If TextBoxItemAjoutQt.Text = "" Or Not Int32.TryParse(TextBoxItemAjoutQt.Text, ItemAjoutQt) Then
+      LabelItemAjoutQt.ForeColor = Color.Red
+      Resultat = False
+    End If
+
+    Return Resultat
+  End Function
+
+  ' Méthode pour mettre les couleurs des labels à la couleur normale
+  Private Sub LabelCouleurNormale()
+    LabelItemAjoutCatNom.ForeColor = Color.Black
+    LabelItemAjoutCodeProduit.ForeColor = Color.Black
+    LabelItemAjoutDepNom.ForeColor = Color.Black
+    LabelItemAjoutDesc.ForeColor = Color.Black
+    LabelItemAjoutEmp.ForeColor = Color.Black
+    LabelItemAjoutFournCode.ForeColor = Color.Black
+    LabelItemAjoutFournNom.ForeColor = Color.Black
+    LabelItemAjoutPrixA.ForeColor = Color.Black
+    LabelItemAjoutPrixV.ForeColor = Color.Black
+    LabelItemAjoutQt.ForeColor = Color.Black
+  End Sub
+
+  Private Sub TextBoxItemAjoutCodeProduit_TextChanged(sender As Object, e As EventArgs) Handles TextBoxItemAjoutCodeProduit.TextChanged
+    LabelItemAjoutCodeProduit.ForeColor = Color.Black
+  End Sub
+
+  Private Sub TextBoxItemAjoutDesc_TextChanged(sender As Object, e As EventArgs) Handles TextBoxItemAjoutDesc.TextChanged
+    LabelItemAjoutDesc.ForeColor = Color.Black
+  End Sub
+
+  Private Sub TextBoxItemAjoutEmp_TextChanged(sender As Object, e As EventArgs) Handles TextBoxItemAjoutEmp.TextChanged
+    LabelItemAjoutEmp.ForeColor = Color.Black
+  End Sub
+
+  Private Sub ComboBoxItemAjoutFournNom_SelectedIndexChanged(sender As Object, e As EventArgs)
+    LabelItemAjoutFournNom.ForeColor = Color.Black
+  End Sub
+
+  Private Sub TextBoxItemAjoutFournCode_TextChanged(sender As Object, e As EventArgs) Handles TextBoxItemAjoutFournCode.TextChanged
+    LabelItemAjoutFournCode.ForeColor = Color.Black
+  End Sub
+
+  Private Sub ComboBoxItemAjoutDepNom_SelectedIndexChanged(sender As Object, e As EventArgs)
+    LabelItemAjoutDepNom.ForeColor = Color.Black
+  End Sub
+
+  Private Sub ComboBoxItemAjoutCatNom_SelectedIndexChanged(sender As Object, e As EventArgs)
+    LabelItemAjoutCatNom.ForeColor = Color.Black
+  End Sub
+
+  Private Sub TextBoxItemAjoutPrixA_TextChanged(sender As Object, e As EventArgs) Handles TextBoxItemAjoutPrixA.TextChanged
+    LabelItemAjoutPrixA.ForeColor = Color.Black
+  End Sub
+
+  Private Sub TextBoxItemAjoutPrixV_TextChanged(sender As Object, e As EventArgs) Handles TextBoxItemAjoutPrixV.TextChanged
+    LabelItemAjoutPrixV.ForeColor = Color.Black
+  End Sub
+
+  Private Sub TextBoxItemAjoutQt_TextChanged(sender As Object, e As EventArgs) Handles TextBoxItemAjoutQt.TextChanged
+    LabelItemAjoutQt.ForeColor = Color.Black
+  End Sub
+
+  Private Sub FormAjoutItem_Closing(ByVal sender As Object,
+    ByVal e As System.ComponentModel.CancelEventArgs) _
+    Handles MyBase.Closing
+    ' Source : https://bytes.com/topic/visual-basic-net/answers/371062-hide-main-form-instead-closing
+    ' Fonction pour cacher le formulaire au lieu de le fermer
+    Me.LabelCouleurNormale()
+    Me.Hide()
+    e.Cancel = True
+  End Sub
+End Class
--- TP1-VB/VariablesGlobales.vb
+++ TP1-VB/VariablesGlobales.vb
@@ -15,17 +15,17 @@
   Public Shared ReadOnly FACT_FAX As String = "418 962-2458"
   Public Shared ReadOnly FACT_COURRIEL As String = "communications@cegepsi.ca"
 
-  Public Shared ReadOnly DROIT_INVENTAIRE_SELECT As Int16 = 1     ' Voir le tab inventaire et le DGV (Contrôle de base)
-  Public Shared ReadOnly DROIT_INVENTAIRE_UPDATE As Int16 = 2     ' Mettre à jour un inventaire
-  Public Shared ReadOnly DROIT_INVENTAIRE_INSERT As Int16 = 4     ' Ajouter un inventaire
-  Public Shared ReadOnly DROIT_INVENTAIRE_DELETE As Int16 = 8     ' Supprimer un inventaire
-  Public Shared ReadOnly DROIT_INVENTAIRE_ADMIN As Int16 = 16     ' Voir l'estimation de l'inventaire et les prix d'achats
-  Public Shared ReadOnly DROIT_FACTURE_SELECT As Int16 = 32       ' Voir le tab facture et le DGV
-  Public Shared ReadOnly DROIT_FACTURE_UPDATE As Int16 = 64       ' Mettre à jour une facture
-  Public Shared ReadOnly DROIT_FACTURE_INSERT As Int16 = 128      ' Ajouter une facture (Contrôle de base)
-  Public Shared ReadOnly DROIT_FACTURE_DELETE As Int16 = 256      ' Supprimer une facture
-  Public Shared ReadOnly DROIT_UTILISATEUR_SELECT As Int16 = 512  ' Voir le tab utilisateur et le DGV
-  Public Shared ReadOnly DROIT_UTILISATEUR_UPDATE As Int16 = 1024 ' Mettre à jour un utilisateur
-  Public Shared ReadOnly DROIT_UTILISATEUR_INSERT As Int16 = 2048 ' Ajouter un utilisateur
-  Public Shared ReadOnly DROIT_UTILISATEUR_DELETE As Int16 = 4096 ' Supprimer un utilisateur
+  Public Shared ReadOnly DROIT_INVENTAIRE_SELECT As Int16 = 1     ' Voir le tab inventaire et le DGV (Contrôle de base)     1   1000 0000 0000 0000 
+  Public Shared ReadOnly DROIT_INVENTAIRE_UPDATE As Int16 = 2     ' Mettre à jour un inventaire                             2   0100 0000 0000 0000
+  Public Shared ReadOnly DROIT_INVENTAIRE_INSERT As Int16 = 4     ' Ajouter un inventaire                                   3   0010 0000 0000 0000
+  Public Shared ReadOnly DROIT_INVENTAIRE_DELETE As Int16 = 8     ' Supprimer un inventaire                                 4   0001 0000 0000 0000
+  Public Shared ReadOnly DROIT_INVENTAIRE_ADMIN As Int16 = 16     ' Voir l'estimation de l'inventaire et les prix d'achats  5   0000 1000 0000 0000
+  Public Shared ReadOnly DROIT_FACTURE_SELECT As Int16 = 32       ' Voir le tab facture et le DGV                           6   0000 0100 0000 0000
+  Public Shared ReadOnly DROIT_FACTURE_UPDATE As Int16 = 64       ' Mettre à jour une facture                               7   0000 0010 0000 0000
+  Public Shared ReadOnly DROIT_FACTURE_INSERT As Int16 = 128      ' Ajouter une facture (Contrôle de base)                  8   0000 0001 0000 0000
+  Public Shared ReadOnly DROIT_FACTURE_DELETE As Int16 = 256      ' Supprimer une facture                                   9   0000 0000 0100 0000
+  Public Shared ReadOnly DROIT_UTILISATEUR_SELECT As Int16 = 512  ' Voir le tab utilisateur et le DGV                       10  0000 0000 0010 0000
+  Public Shared ReadOnly DROIT_UTILISATEUR_UPDATE As Int16 = 1024 ' Mettre à jour un utilisateur                            11  0000 0000 0001 0000
+  Public Shared ReadOnly DROIT_UTILISATEUR_INSERT As Int16 = 2048 ' Ajouter un utilisateur                                  12  0000 0000 0000 1000
+  Public Shared ReadOnly DROIT_UTILISATEUR_DELETE As Int16 = 4096 ' Supprimer un utilisateur                                13  0000 0000 0000 0100
 End Class
